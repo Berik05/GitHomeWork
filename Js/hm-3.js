51aeccb7336844ab10d4c4b579fa42145c9d4077
Berik <beriktalgatuly@gmail.com>
2024-10-30 20:35:10 +0500

    hm-3 week-9

---
+++ Js/hm-3.js
@@ -0,0 +1,10 @@
+let name = "Берік"; // Тип: string
+let age = 19; //Тип: number
+let isStudent = true; // Тип: boolean
+let ageAsString = "19"; //Тип: string
+
+
+console.log("Аты:", name, "| Тип:", typeof name);
+console.log("Жасы:", age, "| Тип:", typeof age);
+console.log("Студент пе:", isStudent, "| Тип:", typeof isStudent);
+console.log("19:" , ageAsString, "| Тип:" , typeof ageAsString);
