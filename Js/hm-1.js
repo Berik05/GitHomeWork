f091f8eaf03c1ec4a8f5c757016007169924bea7
Berik <beriktalgatuly@gmail.com>
2024-11-04 20:51:06 +0500

    HomeWork

---
+++ Js/hm-1.js
@@ -0,0 +1,14 @@
+let age = Number(prompt("Сколько вам лет?"))
+let student = Number(prompt("Вы студент? 1-да, 2-нет"))
+
+alert(age>65 || student==1);
+
+let san = Number(prompt("Введите цифру"))
+
+alert(san>=10 && san<=20);
+
+let a = Number(prompt("Проверка на отрицательные числа. Введите первое число"))
+let b = Number(prompt("Введите второе число"))
+
+alert(a<0 && b>0 || a>0 && b<0)
+
